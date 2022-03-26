8e285a0d5ff3e2da50349372f117bd7853531516
martialmania <martialmania19@gmail.com>
2022-03-26 17:43:46 +0300

    Creating two chatting rooms

--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from matplotlib.style import context
 
 # Create your views here.
 
 
 def home(request):
     return render(request, "chat/home.html")
+
+
+def index(request):
+    return render(request, "chat/index.html")
+
+
+def room(request, room_name):
+    context = {"room_name": room_name}
+    return render(request, "chat/room.html", context)
